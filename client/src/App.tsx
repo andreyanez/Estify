c094010b14d1b8776d98e0886cddf2a14e54794d
andreyanez <andre.yanez22@gmail.com>
2022-06-30 11:53:01 -0500

    feat: added tailwind, basic login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { accessToken, logout } from './spotify.js';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import Login from './pages/login.js';
 
 function ScrollToTop() {
 	const { pathname } = useLocation();
@@ -19,7 +20,20 @@ function App() {
 		setToken(accessToken);
 	}, []);
 
-	return <div className="App">{token}</div>;
+	return (
+		<>
+			<main className="App">
+				{!token ? (
+					<Login />
+				) : (
+					<>
+						<h1 className="text-teal-400">Bienvenido</h1>
+						<button onClick={logout}>Cierra sesión</button>
+					</>
+				)}
+			</main>
+		</>
+	);
 }
 
 export default App;
