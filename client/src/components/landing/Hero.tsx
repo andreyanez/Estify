7aa27496dda52e981e2c45d70d220efa6c8027b4
andreyanez <andre.yanez22@gmail.com>
2022-12-06 15:28:49 -0500

    main page deployed, adding images

--- client/src/components/landing/Hero.tsx
+++ client/src/components/landing/Hero.tsx
@@ -1,8 +1,8 @@
 import { useId } from 'react';
 import { PhoneFrame } from './PhoneFrame';
 
-// import homescreen from './homescreen.png';
-// import homescreenWebp from './homescreen.webp';
+import homescreen from './homescreen.png';
+import homescreenWebp from './homescreen.webp';
 
 const LOGIN_URI =
 	process.env.NODE_ENV !== 'production'
@@ -105,10 +105,10 @@ export const Hero = () => {
 						<BackgroundIllustration className="absolute left-1/2 top-4 h-[1026px] w-[1026px] -translate-x-1/3 stroke-gray-300/70 [mask-image:linear-gradient(to_bottom,white_20%,transparent_75%)] sm:top-16 sm:-translate-x-1/2 lg:-top-16 lg:ml-12 xl:-top-14 xl:ml-0" />
 						<div className="-mx-4 h-[448px] px-9 [mask-image:linear-gradient(to_bottom,white_60%,transparent)] sm:mx-0 lg:absolute lg:-inset-x-10 lg:-top-10 lg:-bottom-20 lg:h-auto lg:px-0 lg:pt-10 xl:-bottom-32">
 							<PhoneFrame>
-								{/* <picture>
+								<picture>
 									<source type="image/webp" srcSet={homescreenWebp} />
 									<img src={homescreen} alt="estify" className="homescreen" />
-								</picture> */}
+								</picture>
 							</PhoneFrame>
 						</div>
 					</div>
