03999f3d02062988d5480276703d6c1a9e6d4e77
André Yañez <andre.yanez22@gmail.com>
2023-01-26 20:50:00 -0500

    fix: removed translation

--- client/src/components/FeaturesText.tsx
+++ client/src/components/FeaturesText.tsx
@@ -3,9 +3,7 @@ export const FeaturesText = () => {
 		<ul>
 			<li>
 				<p>
-					<span>
-						<i>Danceability</i> (Bailabilidad):
-					</span>
+					<span>Bailabilidad:</span>
 					Describe que tan apropiada es el track para bailar, este cálculo esta basado en una
 					combinación de elementos musicales incluyendo tempo, estabilidad de ritmo, fuerza en el
 					beat y constancia de estructura.
@@ -13,9 +11,7 @@ export const FeaturesText = () => {
 			</li>
 			<li>
 				<p>
-					<span>
-						<i>Energy</i> (Energía):
-					</span>
+					<span>Energía:</span>
 					Representa una medida perceptiva de intensidad y actividad. Tipicamente, tracks
 					energeticos se sienten rápidos y estridentes. Por ejemplo: El <i>Death metal</i> tiene
 					alta energía, a diferencia de una composición de Bach. Los elementos que se consideran
@@ -25,9 +21,7 @@ export const FeaturesText = () => {
 			</li>
 			<li>
 				<p>
-					<span>
-						<i>Instrumentalness</i> (Probabilidad de ausencia de letra):
-					</span>
+					<span>Instrumentalidad:</span>
 					Predice si un track no contiene letra. Sonidos como "Ooh" y "Aah" son considerados
 					instrumentos en este contexto. Rap y voces son considerados "letra". Mientras más cercano
 					el valor llegue a 1.0, mayor probabilidad de que el track sea enteramente instrumental.
@@ -36,9 +30,7 @@ export const FeaturesText = () => {
 
 			<li>
 				<p>
-					<span>
-						<i>Valence</i> (Valencia):
-					</span>
+					<span>Valencia:</span>
 					Describe la relativa positividad expresada en el track. Tracks con una valencia alta
 					manifiestan emociones positivas (felicidad, ánimo, eufória), mientras que tracks con
 					valencia manifiestan emociones negativas (tristeza,depresión,enojo). Mientras más cercano
